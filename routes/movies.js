69a783501773a7f2926605487ddae4b220410a85
Lyle Rubin <lylerubin912@gmail.com>
2021-04-16 12:01:35 -0800

    Some refactoring

--- routes/movies.js
+++ routes/movies.js
@@ -3,26 +3,20 @@ const router = express.Router();
 const knex = require("../db/knexConfig");
 
 /* GET users listing. */
-router.get("/", function (req, res, next) {
-  //res.send('this is a list of movies');
-  if (Object.keys(req.query).length === 0) {
-    knex("movies").then((data) => {
-      res.status(200).json(data);
-    });
-  } else {
-    knex("movies")
-      .where({ title: req.query.title })
-      .then((data) => {
-        //must use %20 in the url: http://localhost:3000/movies?title=Midnight%20In%20Paris  midnightinparis
+router.get("/", function (req, res) {
+  Object.keys(req.query).length === 0
+    ? knex("movies").then((data) => {
         res.status(200).json(data);
-      });
-  }
+      })
+    : knex("movies")
+        .where({ title: req.query.title })
+        .then((data) => {//must use %20 in the url: http://localhost:3000/movies?title=Midnight%20In%20Paris
+          res.status(200).json(data);
+        });
 });
 
 router.get("/:movieId", function (req, res) {
-  //res.send('this is a list of movies');
-  console.log(typeof Number.parseInt(req.params.movieId));
-  Number.isNaN(Number.parseInt(req.params.movieId))
+  isNaN(parseInt(req.params.movieId))
     ? res.status(400).send("Invalid ID supplied")
     : knex("movies")
         .where({ id: req.params.movieId })
@@ -34,16 +28,18 @@ router.get("/:movieId", function (req, res) {
 });
 
 router.delete("/:movieId", function (req, res) {
-  Number.isNaN(Number.parseInt(req.params.movieId))
+  isNaN(parseInt(req.params.movieId))
     ? res.status(400).send("Invalid ID supplied")
     : knex("movies")
         .where({ id: req.params.movieId })
         .del()
-        .then(res.send("Movie Deleted"));
+        .then(res.send(`Movie at id ${req.params.movieId} deleted`));
 });
 
 router.post("/", function (req, res) {
-  knex("movies").insert(req.body).then(res.send("Movie Added"));
+  knex("movies")
+    .insert(req.body)
+    .then(res.send(`Movie ${req.body.title} added`));
 });
 
 module.exports = router;
