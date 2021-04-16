38b3c5f605bc20043c754b1d54d457e9898c84ee
Lyle Rubin <lylerubin912@gmail.com>
2021-04-16 08:55:29 -0800

    Created knex movies

---
+++ migrations/20210416163906_create_movies.js
@@ -0,0 +1,13 @@
+exports.up = function(knex) {
+    return knex.schema.createTable('movies', table => {
+        table.increments('id');
+        table.string('title');
+        table.integer('runtime');
+        table.integer('release_year');
+        table.string('director');
+    })
+};
+
+exports.down = function(knex) {
+    return knex.schema.dropTableIfExists('movies');
+};
